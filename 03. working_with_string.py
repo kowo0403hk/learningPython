70ea814b8bf127c06e5fa66b54ab5bb398688cec
Alex Cheung <alexkwokoncheung@AC.local>
2022-11-08 20:49:45 -0800

    updated files

---
+++ 03. working_with_string.py
@@ -0,0 +1,22 @@
+phrase = "Giraffe\nAcademy"
+print(phrase + "\nis cool")
+
+# using string methods
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+
+# chaining methods together
+print(phrase.upper().isupper())
+print(len(phrase))
+
+# working with string index like an array
+print(phrase[1])
+
+# more
+# the index function returns the first index of the character we are searching for. Or, it can return
+# the starting position of the phrase we are looking for
+print(phrase.index("G"))
+print(phrase.index("Acad"))
+
+print(phrase.replace("Giraffe", "Elephant")) 
