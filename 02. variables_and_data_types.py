45835bbfaf3ad08550cddd85b590ccd7b3a7b334
Alex Cheung <alexkwokoncheung@AC.local>
2022-11-08 20:33:25 -0800

    update

---
+++ 02. variables_and_data_types.py
@@ -0,0 +1,14 @@
+character_name = "John"
+character_age = "35"
+is_male = True
+
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+
+character_name = "Mike"
+
+print("He really liked the name " + character_name + ", ")
+print("but he didn't like being " + character_age + ".")
+
+# data types:
+# string, number (including decimal numbers), boolean
---
+++ README.md
@@ -0,0 +1,2 @@
+# Learning Python
+This is a Python learning program provided by freeCodeCamp.org. All credits to https://www.youtube.com/watch?v=rfscVS0vtbw&t=628s. 
